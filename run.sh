929f36dcd7dcd5d42f6f4a1c836d566f25c67852
Michael Mader <m.mader@mairdumont.com>
2016-11-03 15:50:10 +0100

    fixed run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -jar target/kompass-graphhopper-web-0.0.1-SNAPSHOT-jar-with-dependencies.jar jetty.resourcebase=./src/main/webapp jetty.port=8989 jetty.host=localhost config=../kompass-graphhopper/config.properties graph.location=../kompass-graphhopper/europe_alps-gh
+java -jar target/kompass-graphhopper-web-0.0.1-SNAPSHOT-jar-with-dependencies.jar jetty.resourcebase=./src/main/webapp jetty.port=8989 jetty.host=0.0.0.0 config=../kompass-graphhopper/config.properties graph.location=../kompass-graphhopper/europe_alps-gh
